47b5191fe0332d66e4d2348be4ed56d3af99bc0e
abdullah <abdullahaziz723@gmail.com>
2025-07-30 23:17:37 +0530

    bird_velocity

--- src/Flappy_bird.java
+++ src/Flappy_bird.java
@@ -6,7 +6,7 @@ import javax.swing.*;
 import java.util.Random;
 import java.util.ArrayList;
 
-public class Flappy_bird extends JPanel {
+public class Flappy_bird extends JPanel implements ActionListener {
     int boardWidth = 360;
     int boardHeight = 640;
 
@@ -15,6 +15,30 @@ public class Flappy_bird extends JPanel {
     Image birdImg;
     Image topPipeImg;
     Image bottomPipeImg;
+
+    // bird
+    int birdX = boardWidth / 8;
+    int birdY = boardHeight / 2;
+    int birdHeight = 24;
+    int birdWidth = 34;
+
+    class Bird {
+        int x = birdX;
+        int y = birdY;
+        int width = birdWidth;
+        int height = birdHeight;
+        Image img;
+
+        Bird(Image img) {
+            this.img = img;
+        }
+    }
+
+    // game logic
+    Bird bird;
+    int velocityY = -6;
+
+    Timer gameLoop;
 
     Flappy_bird() {
         setPreferredSize(new Dimension(boardWidth, boardHeight));
@@ -26,8 +50,12 @@ public class Flappy_bird extends JPanel {
         topPipeImg = new ImageIcon(getClass().getResource("/src/assets/toppipe.png")).getImage();
         bottomPipeImg = new ImageIcon(getClass().getResource("/src/assets/bottompipe.png")).getImage();
 
-        // setFocusable(true);
+        // bird
+        bird = new Bird(birdImg);
 
+        // game loop
+        gameLoop = new Timer(1000 / 60, this);
+        gameLoop.start();
     }
 
     public void paintComponent(Graphics g) {
@@ -37,9 +65,26 @@ public class Flappy_bird extends JPanel {
     }
 
     public void draw(Graphics g) {
+
+        System.out.println("draw");
         // Draw background
         g.drawImage(backgroundImg, 0, 0, boardWidth, boardHeight, null);
 
+        // bird
+        g.drawImage(bird.img, bird.x, bird.y, bird.width, bird.height, null);
+
+    }
+
+    public void jump() {
+        // bird         
+
+        bird.y += velocityY;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        jump();
+        repaint();
     }
 
 }
